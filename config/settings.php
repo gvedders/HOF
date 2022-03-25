37594f8f61dc383cbea87cdab4950bff583b19db
Greg Vedders <veddegre@aquinas.edu>
2022-03-25 13:46:15 -0400

    Add variable for site live test

--- config/settings.php
+++ config/settings.php
@@ -8,6 +8,9 @@ $dbname = "ath_hof";
 // Number of rows to show per page
 $rowsperpage = 8;
 
+// Display site or coming soon page (true for live)
+$live = "true";
+
 // No result error message
 $noresultmessage = "<td align=\"center\" class=\"underConst\">This section is under construction, please check back soon as new information is added daily.</td></tr></table>";
 
